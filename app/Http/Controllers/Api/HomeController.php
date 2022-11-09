afc446a2dd3808b8b4f5567a43befb65d3c4e43c
hossein <hosseinenshaei@stu.nit.ac.ir>
2022-11-09 22:44:09 +0330

    discount query changed

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -13,20 +13,21 @@ class HomeController extends Controller
 {
 
 
-    public function getProductsByIDs($allProducts, $product_attribute_ids) {
-        $resultProducts = collect([]);
-        foreach ($product_attribute_ids as $pa_id) {
-            $singleProductModel = $allProducts->where('id', $pa_id->product_id);
-            $singleProductModel->load(['attributes' => function ($query) use ($pa_id){
-                $query->where('attributes.id', $pa_id->attribute_id);
-            }]);
-            $resultProducts->push($singleProductModel);
-         }
-         return $resultProducts;
+    public function getProductsByIDs($allProducts, $product_attribute_ids, $filteredBy) {
+        $result = collect([]);
+        foreach($product_attribute_ids as $pa_id) {
+            $product = $allProducts->where('id', $pa_id->product_id)->first();
+            $result->push([
+                'product' => $product, 
+                $filteredBy => $product->attributes->where('id', $pa_id->attribute_id)->first()
+            ]);
+        }
+        return $result;
     }
 
     public function index() {
         $products = Product::get();
+        $products->load('attributes');
 
 
         $pivotMostSale = DB::table('order_product')
@@ -36,49 +37,42 @@ class HomeController extends Controller
         ->take(3)
         ->get();
 
-        $mostSaleProducts = $this->getProductsByIDs($products, $pivotMostSale);
-       
-    
-
-
-
-        $pivotProducts = DB::table('attribute_product')
+        $mostSaleProducts = $this->getProductsByIDs($products, $pivotMostSale, 'most_sale_attribute');
+ 
+        $pivotProductsDesc = DB::table('attribute_product')
         ->select('product_id', 'attribute_id');
 
+
+        $pivotProductsAsc = clone $pivotProductsDesc;
         
-        $pivotMostExpensiveProducts = $pivotProducts
+        $pivotMostExpensiveProducts = $pivotProductsDesc
             ->orderBy('price', 'desc')
             ->take(3)
             ->get();
 
-        $pivotCheapestProducts = $pivotProducts
+        $pivotCheapestProducts = $pivotProductsAsc
             ->orderBy('price', 'asc')
             ->take(3)
             ->get(); 
 
-        $mostExpensiveProducts = $this->getProductsByIDs($products, $pivotMostExpensiveProducts);
-
-        $cheapestProducts = $this->getProductsByIDs($products, $pivotMostExpensiveProducts);
-
-
-
-
-
-        $discountsPriorities = Discount::orderBy('value', 'desc')->pluck('id')->toArray();
-        $pivotDiscounts = DB::table('attribute_product')->get()->toArray();
-
-        $highestDiscounts = array();
-        foreach ($discountsPriorities as $key) {
-
-            foreach ($pivotDiscounts as $img) {
-                if (count($highestDiscounts) === 5){
-                    break 2;
-                }
-                if($img->discount_id == $key) {
-                    $highestDiscounts[] = $img;
-                }
-            }
-
+        $mostExpensiveProducts = $this->getProductsByIDs($products, $pivotMostExpensiveProducts, 'most_expensive_attribute');
+        $cheapestProducts = $this->getProductsByIDs($products, $pivotCheapestProducts, 'cheapest_attribute');
+        
+        $resultProducts = collect([]);
+        $joinResult = DB::table('discounts')
+            ->join('attribute_product', 'discounts.id', '=', 'attribute_product.discount_id')
+            ->select('product_id', 'attribute_id', 'value')
+            ->orderBy('discounts.value', 'desc')
+            ->take(3)
+            ->get();
+        
+        foreach($joinResult as $jr) {
+            $product = $products->where('id', $jr->product_id)->first();
+            $attributes = $product->attributes->where('id', $jr->attribute_id)->first();
+            $resultProducts->push([
+                'product' => $product, 
+                'most_discounted_attribute' => $attributes
+            ]);
         }
 
 
@@ -91,8 +85,8 @@ class HomeController extends Controller
             // 'retrievedProductModels' => $retrievedExpensiveProductModels,
             // 'cheapestProducts' => $cheapestProducts,
             // 'retrievedProductModels' => $retrievedCheapestProductModels,
-            'discountsPriorities' => $discountsPriorities,
-            'orderedDiscounts' => $highestDiscounts,
+            // 'discountsPriorities' => $discountsPriorities,
+            // 'orderedDiscounts' => $highestDiscounts,
         ]);
     }
 
